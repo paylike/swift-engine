3a6e1067022a438c65b0d464fb51aacdd871f9a3
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-05-23 11:59:57 +0200

    feat(hints): made Hints dto public

--- Sources/PaylikeEngine/Model/WebView/Hints.swift
+++ Sources/PaylikeEngine/Model/WebView/Hints.swift
@@ -1,7 +1,6 @@
 /**
- * Used when hints are gathered from the webview window during TDS Flow. It is needed to define a
- * deserializer for the Json format.
+ * Used when hints are gathered from the webview window during TDS Flow. It is needed to define a deserializer for the Json format.
  */
-public struct Hints : Decodable { // @TODO: make it internal
-    public var hints = [String]() // @TODO: make it internal
+public struct Hints : Decodable {
+    public var hints = [String]()
 }
