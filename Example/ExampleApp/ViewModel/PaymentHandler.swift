c8914904b7dab5cdcd5802268c6699930c78fc70
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-06-05 14:35:51 +0200

    fix(applePay): cleanup of temporary applePay flow implementation

--- Example/ExampleApp/ViewModel/PaymentHandler.swift
+++ Example/ExampleApp/ViewModel/PaymentHandler.swift
@@ -19,7 +19,6 @@ class PaymentHandler: NSObject {
     func startPayment(completion: @escaping PaymentCompletionHandler) {
         completionHandler = completion
         
-        // Create our payment request
         let paymentRequest = PKPaymentRequest()
         paymentRequest.paymentSummaryItems = [.init(label: "Test Company recieving the payment", amount: .one, type: .final)]
         paymentRequest.merchantIdentifier = applePayMerchantId
@@ -27,16 +26,13 @@ class PaymentHandler: NSObject {
         paymentRequest.currencyCode = CurrencyCodes.HUF.rawValue
         paymentRequest.merchantCapabilities = [.capability3DS]
         paymentRequest.supportedNetworks = [.visa, .masterCard, .maestro]
-        // paymentRequest.requiredShippingContactFields = [.phoneNumber, .emailAddress]
-
-        // Display our payment request
+        
         paymentController = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
         paymentController?.delegate = self
         paymentController?.present(completion: { (presented: Bool) in
             if presented {
-                NSLog("Presented payment controller")
+                // nothing
             } else {
-                NSLog("Failed to present payment controller")
                 self.completionHandler!(false)
             }
         })
@@ -48,39 +44,17 @@ class PaymentHandler: NSObject {
  */
 extension PaymentHandler: PKPaymentAuthorizationControllerDelegate {
     
-    
-    
-    
     func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
-//        Perform some very basic validation on the provided contact information
-//        if payment.shippingContact?.emailAddress == nil || payment.shippingContact?.phoneNumber == nil {
-//            paymentStatus = .failure
-//        } else {
-//            Here you would send the payment token to your server or payment provider to process
-//            Once processed, return an appropriate status in the completion handler (success, failure, etc)
-//            paymentStatus = .success
-//        }
-        
         Task {
-            
-            debugPrint("applePayToken payment: \(payment)")
-            
             if
-                let token2 = String(data: payment.token.paymentData, encoding: .utf8)
-//                let token = try? JSONDecoder().decode(ApplePayToken.self, from: payment.token.paymentData)
-//                let data = token.data
+                let token = String(data: payment.token.paymentData, encoding: .utf8)
             {
-//                debugPrint("applePayToken token: \(token)")
-//                debugPrint("applePayToken data: \(data)")
-                debugPrint("applePayToken: \(token2)")
-
-                await engine.addEssentialPaymentData(applePayToken: token2)
+                await engine.addEssentialPaymentData(applePayToken: token)
                 engine.addAdditionalPaymentData(textData: "From Apple Pay")
                 engine.addDescriptionPaymentData(paymentAmount: try! PaymentAmount(currency: CurrencyCodes.HUF, double: 1.0), paymentTestData: PaymentTest())
                 await engine.startPayment()
-                NSLog("hello comp in paymentauthorizationController")
             } else {
-                NSLog("hello fail in paymentauthorizationController")
+                engine.prepareError(e: EngineError.NotImplemented)
                 paymentStatus = .failure
             }
             completion(paymentStatus)
@@ -91,18 +65,18 @@ extension PaymentHandler: PKPaymentAuthorizationControllerDelegate {
         controller.dismiss {
             DispatchQueue.main.async {
                 if self.paymentStatus == .success {
-                    NSLog("hello comp in didfinish")
                     self.completionHandler!(true)
                 } else {
-                    NSLog("hello fail in didfinish")
                     self.completionHandler!(false)
                 }
             }
         }
     }
-    
 }
 
+/**
+ * Temporary applePay payload DTO
+ */
 struct ApplePayToken: Decodable {
     let data: String?
     let header: Header?
@@ -110,6 +84,9 @@ struct ApplePayToken: Decodable {
     let version: String?
 }
 
+/**
+ * Temporary applePay payload DTO
+ */
 struct Header: Decodable {
     let publicKeyHash: String?
     let ephemeralPublicKey: String?
